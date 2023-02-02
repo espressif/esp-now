7c96fc49f0ffa9672aa7e8fc6de8c03375da4e3b
Wu Jian Gang <wujiangang@espressif.com>
2023-02-02 11:55:27 +0800

    example: Cleanup provisioning example

--- examples/provisioning/main/app_main.c
+++ examples/provisioning/main/app_main.c
@@ -6,19 +6,10 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/queue.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-
-#include "esp_system.h"
 #include "esp_log.h"
-#include "esp_wifi.h"
 #include "esp_netif.h"
+#include "esp_system.h"
 #include "esp_wifi.h"
 
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 4, 0)
@@ -27,30 +18,33 @@
 
 #include "esp_utils.h"
 #include "esp_storage.h"
+
 #include "espnow.h"
 #include "espnow_prov.h"
 
 static const char *TAG = "app_main";
 
-static void wifi_init()
+static void app_wifi_init()
 {
-    ESP_ERROR_CHECK(esp_netif_init());
-
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+#ifdef CONFIG_APP_ESPNOW_PROV_INITATOR
+    ESP_ERROR_CHECK(esp_netif_init());
     esp_netif_create_default_wifi_sta();
+#endif
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
     ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_start());
 }
 
-#ifdef CONFIG_ESPNOW_PROV_RESPONDER
-static esp_err_t responder_recv_callback(uint8_t *src_addr, void *data,
-                      size_t size, wifi_pkt_rx_ctrl_t *rx_ctrl)
+#ifdef CONFIG_APP_ESPNOW_PROV_RESPONDER
+static esp_err_t app_espnow_prov_recv_cb(uint8_t *src_addr, void *data,
+        size_t size, wifi_pkt_rx_ctrl_t *rx_ctrl)
 {
     ESP_PARAM_CHECK(src_addr);
     ESP_PARAM_CHECK(data);
@@ -62,14 +56,14 @@ static esp_err_t responder_recv_callback(uint8_t *src_addr, void *data,
      * @brief Authenticate the device through the information of the initiator
      */
     ESP_LOGI(TAG, "MAC: "MACSTR", Channel: %d, RSSI: %d, Product_id: %s, Device Name: %s, Auth Mode: %d, device_secret: %s",
-                MAC2STR(src_addr), rx_ctrl->channel, rx_ctrl->rssi,
-                initator_info->product_id, initator_info->device_name,
-                initator_info->auth_mode, initator_info->device_secret);
+             MAC2STR(src_addr), rx_ctrl->channel, rx_ctrl->rssi,
+             initator_info->product_id, initator_info->device_name,
+             initator_info->auth_mode, initator_info->device_secret);
 
     return ESP_OK;
 }
 
-static esp_err_t provisioning_responder()
+static esp_err_t app_responder_init()
 {
     esp_err_t ret = ESP_OK;
     espnow_prov_responder_t responder_info = {
@@ -77,21 +71,21 @@ static esp_err_t provisioning_responder()
     };
     espnow_prov_wifi_t wifi_config = {
         .sta = {
-            .ssid = CONFIG_ESPNOW_WIFI_SSID,
-            .password = CONFIG_ESPNOW_WIFI_PASSWORD,
+            .ssid = CONFIG_APP_ESPNOW_WIFI_SSID,
+            .password = CONFIG_APP_ESPNOW_WIFI_PASSWORD,
         },
     };
 
-    ret = espnow_prov_responder_start(&responder_info, pdMS_TO_TICKS(30 * 1000), &wifi_config, responder_recv_callback);
+    ret = espnow_prov_responder_start(&responder_info, pdMS_TO_TICKS(30 * 1000), &wifi_config, app_espnow_prov_recv_cb);
     ESP_ERROR_RETURN(ret != ESP_OK, ret, "espnow_prov_responder_beacon");
 
     return ESP_OK;
 }
 #endif
 
-#ifdef CONFIG_ESPNOW_PROV_INITATOR
-static esp_err_t initator_recv_callback(uint8_t *src_addr, void *data,
-                      size_t size, wifi_pkt_rx_ctrl_t *rx_ctrl)
+#ifdef CONFIG_APP_ESPNOW_PROV_INITATOR
+static esp_err_t app_espnow_prov_recv_cb(uint8_t *src_addr, void *data,
+        size_t size, wifi_pkt_rx_ctrl_t *rx_ctrl)
 {
     ESP_PARAM_CHECK(src_addr);
     ESP_PARAM_CHECK(data);
@@ -99,16 +93,17 @@ static esp_err_t initator_recv_callback(uint8_t *src_addr, void *data,
     ESP_PARAM_CHECK(rx_ctrl);
 
     espnow_prov_wifi_t *wifi_config = (espnow_prov_wifi_t *)data;
+
     ESP_LOGI(TAG, "MAC: "MACSTR", Channel: %d, RSSI: %d, wifi_mode: %d, ssid: %s, password: %s, token: %s",
-                MAC2STR(src_addr), rx_ctrl->channel, rx_ctrl->rssi,
-                wifi_config->mode, wifi_config->sta.ssid, wifi_config->sta.password, wifi_config->token);
+             MAC2STR(src_addr), rx_ctrl->channel, rx_ctrl->rssi,
+             wifi_config->mode, wifi_config->sta.ssid, wifi_config->sta.password, wifi_config->token);
 
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, (wifi_config_t *)&wifi_config->sta));
     ESP_ERROR_CHECK(esp_wifi_connect());
     return ESP_OK;
 }
 
-static esp_err_t provisioning_initator()
+static esp_err_t app_initator_init()
 {
     esp_err_t ret = ESP_OK;
     wifi_pkt_rx_ctrl_t rx_ctrl = {0};
@@ -126,7 +121,7 @@ static esp_err_t provisioning_initator()
                  MAC2STR(responder_addr), rx_ctrl.channel, rx_ctrl.rssi,
                  responder_info.product_id, responder_info.device_name);
 
-        ret = espnow_prov_initator_send(responder_addr, &initator_info, initator_recv_callback, pdMS_TO_TICKS(3 * 1000));
+        ret = espnow_prov_initator_send(responder_addr, &initator_info, app_espnow_prov_recv_cb, pdMS_TO_TICKS(3 * 1000));
         ESP_ERROR_CONTINUE(ret != ESP_OK, "<%s> espnow_prov_initator_send", esp_err_to_name(ret));
 
         break;
@@ -138,16 +133,16 @@ static esp_err_t provisioning_initator()
 
 void app_main()
 {
-    esp_log_level_set("*", ESP_LOG_INFO);
     esp_storage_init();
-    wifi_init();
+
+    app_wifi_init();
 
     espnow_config_t espnow_config = ESPNOW_INIT_CONFIG_DEFAULT();
     espnow_init(&espnow_config);
 
-#ifdef CONFIG_ESPNOW_PROV_INITATOR
-    provisioning_initator();
-#elif CONFIG_ESPNOW_PROV_RESPONDER
-    provisioning_responder();
+#ifdef CONFIG_APP_ESPNOW_PROV_INITATOR
+    app_initator_init();
+#elif CONFIG_APP_ESPNOW_PROV_RESPONDER
+    app_responder_init();
 #endif
 }
