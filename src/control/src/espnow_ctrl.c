229782df90e102a3076e4cdfccea8d9267b5a000
Nebojsa Stojiljkovic <nebojsa.stojiljkovic@ars-es.com>
2023-06-29 14:40:27 +0200

    Fix incorrect condition check for accepting bind request

--- src/control/src/espnow_ctrl.c
+++ src/control/src/espnow_ctrl.c
@@ -198,7 +198,7 @@ static esp_err_t espnow_ctrl_responder_bind_process(uint8_t *src_addr, void *dat
             bind_cb_flag = g_bindlist.cb(ctrl_data->initiator_attribute, src_addr, rx_ctrl->rssi);
         }
 
-        if (bind_cb_flag || esp_log_timestamp() < g_bindlist.timestamp || rx_ctrl->rssi > g_bindlist.rssi) {
+        if (bind_cb_flag && esp_log_timestamp() < g_bindlist.timestamp && rx_ctrl->rssi > g_bindlist.rssi) {
             ESP_LOGI("control_func", "addr: "MACSTR", initiator_type: %d, initiator_value: %d",
                         MAC2STR(src_addr), ctrl_data->initiator_attribute >> 8, ctrl_data->initiator_attribute & 0xff);
 
